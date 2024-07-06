23c1a21c8f38e971e277a0a7f1908d5c877e34db
Marc Byndas <byndasma@gmail.com>
2024-07-06 15:23:52 -0400

    git init

---
+++ src/main/java/com/lc/itinera/ItineraApplication.java
@@ -0,0 +1,13 @@
+package com.lc.itinera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lc/itinera/ItineraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.itinera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
