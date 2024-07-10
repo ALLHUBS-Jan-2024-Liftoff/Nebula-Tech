2e2ee6ffabe62ebb3e0145be223cf5355facf1d6
Marc Byndas <byndasma@gmail.com>
2024-07-10 13:06:29 -0400

    demo ready

--- src/main/java/com/lc/itinera/controllers/DemoController.java
+++ src/main/java/com/lc/itinera/controllers/DemoController.java
@@ -2,20 +2,18 @@ package com.lc.itinera.controllers;
 
 import com.lc.itinera.dto.DemoRequest;
 import com.lc.itinera.dto.DemoResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class DemoController {
 
-	@GetMapping("/api/demo")
+	@GetMapping("/demo")
 	public DemoResponse getDemo() {
 		return new DemoResponse("Hello", "World");
 	}
 
-	@PostMapping("/api/demo")
+	@PostMapping("/demo")
 	public DemoResponse createDemo(@RequestBody DemoRequest request) {
 		return new DemoResponse(request.getName(), request.getDescription());
 	}
