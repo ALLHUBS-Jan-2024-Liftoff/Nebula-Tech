26b415080ce5b553b6af7752f111d4392906c9ca
Marc Byndas <byndasma@gmail.com>
2024-08-13 20:57:08 -0400

    max

--- src/main/java/org/nebula_tech/itinera/controllers/DemoController.java
+++ src/main/java/org/nebula_tech/itinera/controllers/DemoController.java
@@ -1,5 +1,6 @@
 package org.nebula_tech.itinera.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.nebula_tech.itinera.dto.DemoRequest;
 import org.nebula_tech.itinera.dto.DemoResponse;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,8 @@ public class DemoController {
 	}
 
 	@PostMapping("/demo") // returns demo form input values
-	public DemoResponse createDemo(@RequestBody DemoRequest request) {
+	public DemoResponse createDemo(@RequestBody DemoRequest request, HttpSession session) {
+		System.out.println(session.getId());
 		return new DemoResponse(request.getName(), request.getDescription());
 	}
 }
