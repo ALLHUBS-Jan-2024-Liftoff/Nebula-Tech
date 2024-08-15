f062a9cc730de23091800aab72dd91289437f6b8
maxwellmaier <154074391+maxwellmaier@users.noreply.github.com>
2024-08-15 12:46:31 -0500

    Revert "backend user registration working"

--- src/main/java/org/nebula_tech/itinera/repositories/TripRepository.java
+++ src/main/java/org/nebula_tech/itinera/repositories/TripRepository.java
@@ -1,9 +1,9 @@
-//package org.nebula_tech.itinera.repositories;
-//import org.nebula_tech.itinera.models.Trip;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface TripRepository extends CrudRepository<Trip, Integer> {
-//
-//
-//
-//}
+package org.nebula_tech.itinera.repositories;
+import org.nebula_tech.itinera.models.Trip;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TripRepository extends CrudRepository<Trip, Integer> {
+
+
+
+}
--- src/main/java/org/nebula_tech/itinera/repositories/UserRepository.java
+++ src/main/java/org/nebula_tech/itinera/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package org.nebula_tech.itinera.repositories;
-
-
 import org.nebula_tech.itinera.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
 }
