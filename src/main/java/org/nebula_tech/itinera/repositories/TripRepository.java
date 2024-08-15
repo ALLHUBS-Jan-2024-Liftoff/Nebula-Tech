a74b23f8a075b7aacd9c2f20e5533cbb89960891
maxwellmaier <maxwellmaier89@gmail.com>
2024-08-15 18:15:10 -0500

    removed model/trip and repository/tripRepository

--- src/main/java/org/nebula_tech/itinera/repositories/TripRepository.java
+++
@@ -1,9 +0,0 @@
-//package org.nebula_tech.itinera.repositories;
-//import org.nebula_tech.itinera.models.Trip;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface TripRepository extends CrudRepository<Trip, Integer> {
-//
-//
-//
-//}
