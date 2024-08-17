e34966b70b72fe507deb5024b1ad23bf7a7c76d7
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-08-17 00:09:54 -0500

    removed containe4r from faqheader

--- src/main/react/src/pages/FAQHeader.jsx
+++ src/main/react/src/pages/FAQHeader.jsx
@@ -5,9 +5,6 @@ const FAQHeader = () => {
     return (
         <header style={{backgroundImage: "url('https://www.emeraldislerealty.com/wp-content/uploads/2019/12/faq-in-sand-1366x380.jpg')"}}>
             <Navbar bg="dark" varient="dark" style={{backgroundColor: 'transparent'}}>
-                <Container>
-                    <Navbar.Brand style={{color:"white"}}>Itinera</Navbar.Brand>
-                </Container>
             </Navbar>
             <div className='text-center p-5' style={{color:'white'}}>
                 <h1>Frequently Asked Questions</h1>
