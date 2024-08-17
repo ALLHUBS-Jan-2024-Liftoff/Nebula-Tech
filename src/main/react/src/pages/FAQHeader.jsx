403dc468767950068b0e81034ee61c47d6d6fe6c
luciemariec <132121627+luciemariec@users.noreply.github.com>
2024-08-16 23:16:13 -0500

    added styling to faq header

--- src/main/react/src/pages/FAQHeader.jsx
+++ src/main/react/src/pages/FAQHeader.jsx
@@ -3,13 +3,13 @@ import {Navbar, Container} from 'react-bootstrap';
 
 const FAQHeader = () => {
     return (
-        <header>
-            <Navbar>
+        <header style={{backgroundImage: "url('URL GOES HERE')"}}>
+            <Navbar bg="dark" varient="dark" style={{backgroundColor: 'transparent'}}>
                 <Container>
-                    <Navbar.Brand>Itinera</Navbar.Brand>
+                    <Navbar.Brand style={{color:"white"}}>Itinera</Navbar.Brand>
                 </Container>
             </Navbar>
-            <div>
+            <div className='text-center p-5' style={{color:'white'}}>
                 <h1>Frequently Asked Questions</h1>
             </div>
         </header>
