f79b8595ab3bdd1eee2681107bab3e47e3ce71ac
Marc Byndas <byndasma@gmail.com>
2024-08-19 13:20:34 -0400

    purchased trip details now within booking model

--- src/main/java/org/nebula_tech/itinera/controllers/BookingController.java
+++ src/main/java/org/nebula_tech/itinera/controllers/BookingController.java
@@ -1,5 +1,6 @@
 package org.nebula_tech.itinera.controllers;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.stripe.model.CustomerCollection;
@@ -9,6 +10,7 @@ import org.nebula_tech.itinera.models.Trip;
 import org.nebula_tech.itinera.repositories.BookingRepository;
 import org.nebula_tech.itinera.repositories.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -22,29 +24,26 @@ public class BookingController {
     @Autowired
     private TripRepository tripRepository;
 
+    @Value("${STRIPE_API_KEY}")
+    private String stripeApiKey;
+
     @GetMapping("/all-bookings")
     public List<Booking> getBookings() {
         return (List<Booking>) bookingRepository.findAll();
     }
+
     @GetMapping("/bookings")
     public List<Booking> getBookings(@RequestParam String email) throws StripeException {
-        System.out.println(email);
+        // todo: get email from session.getAttribute(userEmail);
+        Stripe.apiKey = stripeApiKey;
         CustomerCollection customers = Customer.list(
             CustomerListParams.builder()
                 .setEmail(email)
                 .build()
         );
         Customer customer = customers.getData().isEmpty() ? null : customers.getData().get(0);
-        System.out.println(customer);
         if (customer != null) {
-            String customerId = customer.getId();
-            List<Booking> bookings = bookingRepository.findByCustomerId(customerId);
-            for (Booking booking : bookings) {
-                System.out.println(booking.getTripId());
-                Trip trip = tripRepository.findByTripId(booking.getTripId());
-                booking.setTripTitle(trip.getTitle());
-            }
-            return bookings;
+            return bookingRepository.findByCustomerId(customer.getId());
         }
         return null;
     }
--- src/main/java/org/nebula_tech/itinera/repositories/BookingRepository.java
+++ src/main/java/org/nebula_tech/itinera/repositories/BookingRepository.java
@@ -1,7 +1,9 @@
 package org.nebula_tech.itinera.repositories;
 
 import org.nebula_tech.itinera.models.Booking;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
