e7c92ab64a993fbff1b45708feb0b3f10172ff01
maxwellmaier <maxwellmaier89@gmail.com>
2024-08-22 07:17:15 -0500

    wishlist uses trip cards like explore page

--- src/main/react/src/components/explore/ExploreCard.jsx
+++ src/main/react/src/components/explore/ExploreCard.jsx
@@ -1,11 +1,12 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import './ExploreCard.css';
 
-function ExploreCard({ title, text, image, id }) {
+function ExploreCard({ title, text, image, id, small }) {
     return (
-        <Card className="h-100">
-            <Card.Img variant="top" src={image} />
+        <Card className={`h-100 ${small ? 'small-card' : ''}`}>
+            <Card.Img variant="top" src={image} className={small ? 'small-card-img' : ''} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{text}</Card.Text>
